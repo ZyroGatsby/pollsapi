b25c78530bea91ff506b8ef1244e00f5c3030f1a
earth <zyro.auson@gmail.com>
2021-01-13 04:53:35 +0100

    new changes

--- core/settings/local.py
+++ core/settings/local.py
@@ -27,7 +27,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
-        'TEST': {
+        'TEST': { 
             'NAME': 'mytestdatabase',
         },
     }
